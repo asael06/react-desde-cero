82a7f17cc598e933e5e390ac350b7cd04eea1ab0
Asael Martínez <asaelmartinez@Asaels-MacBook-Pro.local>
2021-05-20 22:44:40 -0600

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import './styles/styles.scss';
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Fragment>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="banner" src="https://www.ocean-net.es/catalogo/wp-content/uploads/2014/12/img_banner-thin_mountains-2.jpg" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Título del banner</p>
+            <p> Subtítulo del banner</p>
+            <a href="https://ed.team" className="button">Botón del banner</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    <div className="ed-grid m-grid-3"> 
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+    
+  </Fragment>
+)
+
 
 export default App;
